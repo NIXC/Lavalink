5e76253c4c8041ef34a364533092821beed0f84a
NINXC <16856738+NIXC@users.noreply.github.com>
2019-12-06 21:33:51 -0700

    revert magma

--- LavalinkServer/src/main/java/lavalink/server/player/Player.java
+++ LavalinkServer/src/main/java/lavalink/server/player/Player.java
@@ -32,13 +32,12 @@ import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame;
 import lavalink.server.io.SocketContext;
 import lavalink.server.io.SocketServer;
-import net.dv8tion.jda.api.audio.AudioSendHandler;
+import net.dv8tion.jda.core.audio.AudioSendHandler;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
-import java.nio.ByteBuffer;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
@@ -156,8 +155,8 @@ public class Player extends AudioEventAdapter implements AudioSendHandler {
     }
 
     @Override
-    public ByteBuffer provide20MsAudio() {
-        return ByteBuffer.wrap(lastFrame.getData());
+    public byte[] provide20MsAudio() {
+        return lastFrame.getData();
     }
 
     @Override
